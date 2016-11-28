29029fe93c46b856da3d1baf97277f4b737a4533
51jk <tianyaoqi@51jk.com>
2016-11-28 11:08:13 +0800

    测试

--- TestDemo/LegalVerify.m
+++ TestDemo/LegalVerify.m
@@ -13,7 +13,9 @@
 + (BOOL)checkTel:(NSString *)str{
     NSString *regex = @"^[1][3-8]+\\d{9}";//手机号码变化大，所以没有做更精确
     NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
-    return  [pred evaluateWithObject:str];;
+    return  [pred evaluateWithObject:str];
+    //  测试git
+    NSLog(@"aaaaaaaa ====== test");
 }
 
 @end
